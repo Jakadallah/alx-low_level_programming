d077e56c4b68d414179e0b0a774dd41e242fbfbd
unknown <nashonomondi010@gmail.com>
2022-03-13 23:44:21 +0300

    a C program that prints exactly "Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+[A[C[C[C[C[C[C[C#include <>[Dstdio.h>
+
+/**
+*main -Entry point
+*[A[3~[3~[3~[3~[3~[3~[Cmain -Entry point[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+* Return: Always 0()[DSuccess[C[D)
+*/
+
+int main()[Dvoid)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return ()[D0)[0[C;
+}
