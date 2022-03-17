9b3252e3ec2b54769a5ec0c4f10de8deb89a0a24
unknown <nashonomondi010@gmail.com>
2022-03-18 00:14:50 +0300

    main.h file pushing

--- 0x03-debugging/0-main.c
+++
@@ -1,3 +0,0 @@
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 positive_or_negative.c main.c
-carrie@ubuntu:/debugging$ ./a.out
-98 is positive
--- main.h
+++ main.h
@@ -1,12 +1,15 @@
+#include""[Dmain.h[C[C[D[D"[D[D[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[C[C#include ""[Dmain.h"
 
-#ifndef MAIN_H
-#define MAIN_H
+/**
+*main - tests functions that prints if interger is positive or negative[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D
+*Return: 0
+*/
 
-#include <stdio.h>
+int main()[Dvoid)
+{
+ int i;
+ i= 0;
+ positive_or_negative(i);
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-
-#endif /* MAIN_H */
+ return (0);
+}
