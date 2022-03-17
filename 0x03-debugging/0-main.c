94756d53d17c916f92afdf38285bfe63a0935143
unknown <nashonomondi010@gmail.com>
2022-03-17 23:17:42 +0300

    main.c files pushing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,16 @@
-carrie@ubuntu:/debugging$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 positive_or_negative.c 0-main.c -o 0-main
-carrie@ubuntu:/debugging$ ./0-main
-0 is zero
-carrie@ubuntu:/debugging$ wc -l 0-main.c
-16 1-main.c
-carrie@ubuntu:/debugging$ 
+#include "holberton.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
