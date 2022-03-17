302808297da38d508374c853996ff61eee0e9fbd
unknown <nashonomondi010@gmail.com>
2022-03-18 00:25:00 +0300

    main.h file pushing

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*main - tests functions that prints if interger is positive or negative[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+*Return: 0
+*/
+int main(void)
+{
+ int i;
+
+ i= 0;
+ positive_or_negative(i);
+
+ return (0);
+}
